70e528350f5b5d5edb347bf8d2fcb8a31e8d3618
Maya Berchin <mayaberchin@gmail.com>
2026-09-09 13:11:03 -0400

    hunting internal server err bug. fix session['email'] -> session['user']['email'] in render_posts_page, add a settings template

--- app/__init__.py
+++ app/__init__.py
@@ -184,22 +184,22 @@ def skills():
 
 
 # ------------------ POST PAGES ------------------
-
+@login_required
 def render_post_page(page):
     page_info = POST_PAGE_INFO[page]
-    can_post = page != "announcements" or data.is_stuy_teacher(session["email"])
+    can_post = page != "announcements" or data.is_stuy_teacher(session['user']['email'])
     return render_template(
         "post_page.html",
-        page_title=page_info["page_title"],
-        page_description=page_info["page_description"],
-        selected_post_type=page_info["selected_post_type"],
-        new_post_label=page_info["new_post_label"],
+        page_title=page_info['page_title'],
+        page_description=page_info['page_description'],
+        selected_post_type=page_info['selected_post_type'],
+        new_post_label=page_info['new_post_label'],
         can_post=can_post,
-        current_user_email=session["email"]
+        current_user_email=session['email']
     )
 
 @app.route("/announcements")
-@login_required
+#login_required
 def announcements():
     return render_post_page("announcements")
 
